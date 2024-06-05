da651dd04f2acdd64517526d93fd1b42a2103e5e
OIOIO9993 <53928487+GalvanLuis@users.noreply.github.com>
2024-06-05 11:20:12 -0600

    Update auto_metadiversity_index.sh

--- auto_metadiversity_index.sh
+++ auto_metadiversity_index.sh
@@ -2,11 +2,14 @@
 #Es recomendable respaldar el MD_results.csv que ya este creado porque agrega entradas entonces puede haber redundancia
 
 #rm -f MD_results.csv
+path=$1
+
 touch MD_results.csv
 echo "SAMPLE,CONTIGS,RICHNESS,SHANNON,SIMPSON,LOG10_MD,MDI" > ./MD_results.csv
 
-for filename in /dock/*.faa; do
+for filename in $path*.faa; do
 	python /MD_index/metadiversity_index.py -i $filename -r F
 	done
+mv ./MD_results.csv $path
 
 echo "Trabajo Finalizado"
